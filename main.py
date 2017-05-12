9b1f6e2a5e56b12db2023fcb3ae770cf35e2e8a0
ljpi <lesterpi92@gmail.com>
2017-05-11 21:14:55 -0700

    data reading

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import praw
+import datetime
+from praw.models import MoreComments
 
 client_id = open("client_id").read()
 client_secret = open("client_secret").read()
@@ -16,10 +18,48 @@ reddit = praw.Reddit(client_id=client_id,
 
 
 print("US News Top 10 All Time")
-for submission in reddit.subreddit('USNEWS').top(limit=10):
-    print(submission.title)
 
 
-print("World News Top 10 All Time")
-for submission in reddit.subreddit('worldnews').top(limit=10):
-    print(submission.title)
+def getNews(subreddit, n):
+    results=list()
+    dic=dict()
+    commentDic=dict()
+    for submission in reddit.subreddit(subreddit).top(limit=n):
+        tempList=list()
+        tempList.append(submission.fullname)
+        tempList.append(submission.title)
+        tempList.append(datetime.datetime.fromtimestamp(submission.created))
+        tempList.append(submission.score)
+        tempList.append(submission.url)
+        tempList.append(submission.comments.list())
+        commentDicTemp=dict()
+        for comment in submission.comments:
+            if isinstance(comment, MoreComments):
+                continue
+            else:
+                commentDicTemp[comment.id]=comment.body
+                commentDic[comment.id]=comment.body
+        tempList.append(commentDicTemp)
+        dic[submission.id]=tempList
+    results.append(dic)
+    results.append(commentDic)
+    return results
+
+
+startTime = datetime.datetime.now()
+
+resultsUS = getNews("news",1)
+usNews = resultsUS[0]
+usComments = resultsUS[1]
+
+resultsWorld = getNews("worldnews",1)
+worldNews = resultsWorld[0]
+worldComments = resultsWorld[1]
+
+endTime = datetime.datetime.now()
+
+print(startTime)
+print(endTime)
+#10 records ~80 seconds
+#100 records ~800 seconds
+print((endTime-startTime).seconds)
